b99e113c5314b0f529dfbc4c7d8bfb340984c2f4
SamLuar <anamvs987@gmail.com>
2026-08-27 08:24:12 +0100

    Add manifest and service worker to run in Android Tablet

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'wip-cache-v1';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/data/hours.json',
+  '/data/projects.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/public/styles.css',
+  '/public/app.js',
+  '/public/index.html',
+];
+
+//Instalação:
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+//Ativação
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+        return Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map((key) => caches.delete(key))
+        );
+    })
+  );
+  self.clients.claim();
+});
+
+//pedidos
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+        return fetch(event.request);
+    })
+  );
+});
